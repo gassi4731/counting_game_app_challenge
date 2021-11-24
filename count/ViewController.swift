c25cff8594f81f901250bf3e5e4eabbe7649f083
Yo Higashida <penguin4731@gmail.com>
2021-11-24 22:31:26 +0900

    add: mission2

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count: Int = 0
+    
+    @IBOutlet var countLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plus() {
+        count += 1
+    }
 }
 
